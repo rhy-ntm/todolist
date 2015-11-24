4f6f3cdd006d29a8e92703a00a45fe37f0f2797f
natsumi <kat_tun_jinkazu_love@yahoo.co.jp>
2015-11-24 15:44:17 +0900

    mokuhyou4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,8 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
+    @filter = get_filter
+    @name = @filter[:name]
   end
 
   # GET /tasks/1
@@ -60,15 +62,35 @@ class TasksController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def search
+    @tasks = Task.where(" name LIKE '%#{params['search']['name']}%' ")
+    render :index
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
-      params.require(:task).permit(:due_date, :category_id, :name, :done, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def task_params
+    params.require(:task).permit(:due_date, :category_id, :name, :done, :user_id)
+  end
+  
+  #検索フィールドの値をセッションから取得
+  def get_filter
+    local_session["filter"] ||= {}
+  end
+
+  #検索フィールドの値をセッションに設定
+  def set_filter(filter)
+    local_session["filter"]=filter
+  end
+    
+  #このコントローラ内部だけで使うセッションを返す
+  def local_session
+    session[self.class.name] ||= {}
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :auth
 
   # GET /users
   # GET /users.json
@@ -62,13 +63,21 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:userid, :username, :password)
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:userid, :username, :password)
+  end
+  
+  def auth
+    name = 'izumo'
+    passwd = 'syogyo'
+    authenticate_or_request_with_http_basic('Railbook') do |n,p|
+      n == name && p == passwd
     end
+  end
 end
